9bd3d21b91e23b8210185cf73520c63d95c94666
Cat Tran <trandinhcat@gmail.com>
2016-01-19 18:26:16 -0600

    Defined: SQLlite database schema

---
+++ app/src/main/java/com/sweet_roll/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,16 @@
+package com.sweet_roll.android.criminalintent.database;
+
+/**
+ * Created by trand_000 on 1/19/2016.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public static final String NAME= "crimes";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
